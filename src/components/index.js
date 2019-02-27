fb518e3b2ca1d62b9f32213cb3aae64d738d4ff5
sdjdd <sdjddrt@gmail.com>
2019-02-27 10:24:14 +0800

    帖子分页

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Message from './Message'
 import Modal from './Modal'
+import Pagination from './Pagination'
 
 const component = {
     Message,
     Modal,
+    Pagination,
 }
 
 for (const [key, value] of Object.entries(component)) {
